b6c96c521cb54f184aab135a0e4a907481c87912
rigo1012 <rodrigodomingo764@gmail.com>
2023-02-11 01:10:31 -0600

    Initial commit

---
+++ src/Numeros/MayorMenor.java
@@ -0,0 +1,43 @@
+package Numeros;
+
+import java.util.Scanner;
+
+public class MayorMenor {
+    public  static void main (String[] args){
+        Scanner abc=new Scanner(System.in);
+        int num, nume, numero;
+        System.out.println("NUMEROS ORDENADOS");
+        System.out.println("Ingrese tres numeros");
+        num= abc.nextInt();
+        nume= abc.nextInt();
+        numero=abc.nextInt();
+
+        if(num>nume){
+            if(num>numero){
+                System.out.println("Numero mayor es" + num);
+                if(nume>numero){
+                    System.out.println("Intermedio es" + nume);
+                    System.out.println("Menor es"+ numero);
+                }else{
+                    System.out.println("Numero mayor es" + num);
+                    System.out.println("Intermedio es" + numero);
+                    System.out.println("Menor es" + nume);
+                }
+            }else{
+                System.out.println("Numero mayor es" + numero);
+                System.out.println("Intermedio es" + num);
+                System.out.println("Menor es" + nume);
+            }
+        }else{
+          if(nume>numero){
+              System.out.println("Numero mayor es" + nume);
+              System.out.println("Intermedio es" + numero);
+              System.out.println("Menor es" + num);
+          }else{
+              System.out.println("El numero mayor es" + numero);
+              System.out.println("Intermedio es" + nume);
+              System.out.println("El menor es" + num);
+          }
+        }
+    }
+}
